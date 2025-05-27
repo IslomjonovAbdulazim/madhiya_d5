64733ab3775f0c7c39dd985b802e21acb335f9e6
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-27 18:15:56 +0500

    imp

--- lib/pages/play_page.dart
+++ lib/pages/play_page.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:madhiya_d5/main.dart';
 
 class PlayPage extends StatefulWidget {
-  const PlayPage({super.key});
+  final SongModel song;
+  const PlayPage({super.key, required this.song});
 
   @override
   State<PlayPage> createState() => _PlayPageState();
 }
 
 class _PlayPageState extends State<PlayPage> {
-
-
+  final player = AudioPlayer();
 
   @override
   Widget build(BuildContext context) {

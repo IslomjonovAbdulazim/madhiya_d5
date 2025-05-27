362d7f4e4fede3e36841c3b5c092a0178475bcab
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-27 19:01:35 +0500

    imp

--- lib/pages/play_page.dart
+++ lib/pages/play_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:madhiya_d5/main.dart';
@@ -25,6 +26,8 @@ class _PlayPageState extends State<PlayPage> {
     await player.setAsset(widget.song.asset);
   }
 
+
+
   @override
   void dispose() {
     player.dispose();
@@ -33,6 +36,73 @@ class _PlayPageState extends State<PlayPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  widget.song.title,
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  widget.song.title.substring(0, 4),
+                  style: TextStyle(fontSize: 200),
+                ),
+
+                // slider
+                StreamBuilder<Duration>(
+                    stream: player.positionStream,
+                    builder: (context, snapshot) {
+                      final pos =
+                          (snapshot.data ?? Duration.zero).inMilliseconds;
+                      return Slider(
+                        value: pos.toDouble(),
+                        min: 0,
+                        max: player.duration?.inMilliseconds.toDouble() ?? 0,
+                        onChanged: (value) {
+                          player.seek(Duration(milliseconds: value.toInt()));
+                        },
+                      );
+                    }),
+
+                // controller
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    StreamBuilder<bool>(
+                        stream: player.playingStream,
+                        builder: (context, snapshot) {
+                          final isPlaying = snapshot.data == true;
+                          return CupertinoButton(
+                            onPressed: () {
+                              if (isPlaying) {
+                                player.pause();
+                              } else {
+                                player.play();
+                              }
+                            },
+                            child: Icon(
+                              isPlaying
+                                  ? CupertinoIcons.pause_fill
+                                  : CupertinoIcons.play_arrow_solid,
+                              size: 60,
+                            ),
+                          );
+                        }),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
